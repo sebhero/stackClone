871aeb93a84a257ec733d6119d2de7a2338d0f68
seb <trevligt.att.ses@gmail.com>
2015-08-10 20:27:08 +0200

    started working on Roles

--- server/startUp/startUp.js
+++ server/startUp/startUp.js
@@ -2,10 +2,34 @@
  * Created by sebadmin on 2015-08-06.
  */
 ///<reference path='../../typingsCustom/myInterfaces.d.ts' />
-var id = Accounts.createUser({
-    email: "seb@1.com",
-    password: "test",
-    profile: { name: 'seb' }
+Meteor.startup(function () {
+    //Check if admin user exists
+    if (Meteor.users.findOne({ "emails.address": "seb@1.com" })) {
+        //Save away user
+        var user = Meteor.users.findOne({ "emails.address": "seb@1.com" });
+        //do nottin
+        console.log("admin user exists " + user.emails[0].address);
+        //Check if user has admin role
+        if (Roles.userIsInRole(user, 'admin') == false) {
+            console.log("adding role admin");
+            Roles.addUsersToRoles(user, 'admin');
+        }
+        //printe roles
+        console.log(Roles.getRolesForUser(user));
+    }
+    else {
+        console.log("Create user");
+        //Create User
+        Accounts.createUser({
+            username: 'seb',
+            email: 'seb@1.com',
+            password: 'test',
+            profile: {}
+        });
+    }
+    //show users
+    Meteor.users.find().forEach(function (usr) {
+        console.log(usr.emails);
+    });
 });
-Roles.addUsersToRoles(id, 'admin');
 //# sourceMappingURL=startUp.js.map
